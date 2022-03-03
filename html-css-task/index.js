6114a6f623331e055e708bce39d23824c01d5494
Danilamaimeskul <dan.maika@mail.ru>
2022-03-04 02:53:58 +0300

    Added header display from array of objects in JS

---
+++ html-css-task/index.js
@@ -0,0 +1,274 @@
+const headerMenu = [
+    {
+        title: "Why Spring?",
+        dropdown: [
+            {
+                href: '#',
+                text: 'Overview',
+            },
+            {
+                href: '#',
+                text: 'Microservices',
+            },
+            {
+                href: '#',
+                text: 'Reactive',
+            },
+            {
+                href: '#',
+                text: 'Event Driven',
+            },
+            {
+                href: '#',
+                text: 'Cloud',
+            },
+            {
+                href: '#',
+                text: 'Web Applications',
+            },
+            {
+                href: '#',
+                text: 'Serverless',
+            },
+            {
+                href: '#',
+                text: 'Batch',
+            },
+        ],
+    },
+    {
+        title: "Learn",
+        dropdown: [
+            {
+                href: '#',
+                text: 'Overview',
+            },
+            {
+                href: '#',
+                text: 'Quickstart',
+            },
+            {
+                href: '#',
+                text: 'Guides',
+            },
+            {
+                href: '#',
+                text: 'Blog',
+            },
+        ],
+    },
+    {
+        title: "Projects",
+        dropdown: [
+            {
+                href: '#',
+                text: 'Overview',
+            },
+            {
+                href: '#',
+                text: 'Spring Boot',
+            },
+            {
+                href: '#',
+                text: 'Spring Framework',
+            },
+            {
+                href: '#',
+                text: 'Spring Cloud',
+            },
+            {
+                href: '#',
+                text: 'Spring Cloud Data Flow',
+            },
+            {
+                href: '#',
+                text: 'Spring Data',
+            },
+            {
+                href: '#',
+                text: 'Spring Integration',
+            },
+            {
+                href: '#',
+                text: 'Spring Batch',
+            },
+            {
+                href: '#',
+                text: 'Spring Security',
+            },
+            {
+                href: '#',
+                text: 'View all projects',
+            },
+
+        ],
+    },
+    {
+        title: "Training",
+        href: "#",
+    },
+    {
+        title: "Support",
+        href: "#",
+    },
+    {
+        title: "Community",
+        dropdown: [
+            {
+                href: '#',
+                text: 'Overview',
+            },
+            {
+                href: '#',
+                text: 'Events',
+            },
+            {
+                href: '#',
+                text: 'Team',
+            },
+
+        ],
+    },
+];
+const projects = [
+    {
+        title: "Spring Boot",
+        description: "Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible",
+        imageSrc: "assets/images/card-images/spring-boot.svg",
+    },
+    {
+        title: "Spring Framework",
+        description: "Provides core support for dependency injection, transaction management, web apps, data access, messaging, and more.",
+        imageSrc: "assets/images/card-images/spring-framework-2.svg",
+    },
+    {
+        title: "Spring Data",
+        description: "Provides a consistent approach to data access – relational, non-relational, map-reduce, and beyond.",
+        imageSrc: "assets/images/card-images/spring-data-3.svg",
+    },
+    {
+        title: "Spring Cloud",
+        description: "Provides a set of tools for common patterns in distributed systems. Useful for building and deploying microservices.",
+        imageSrc: "assets/images/card-images/spring-cloud-4.svg",
+    },
+    {
+        title: "Spring Cloud Data Flow",
+        description: "Provides an orchestration service for composable data microservice applications on modern runtimes.",
+        imageSrc: "assets/images/card-images/spring-data-flow-5.svg",
+    },
+    {
+        title: "Spring Security",
+        description: "Protects your application with comprehensive and extensible authentication and authorization support.",
+        imageSrc: "assets/images/card-images/spring-security-6.svg",
+    },
+    {
+        title: "Spring for GraphQL",
+        description: "Spring for GraphQL provides support for Spring applications built on GraphQL Java.",
+        imageSrc: "assets/images/card-images/spring-graphql-7.svg",
+    },
+    {
+        title: "Spring Session",
+        description: "Provides an API and implementations for managing a user’s session information.",
+        imageSrc: "assets/images/card-images/logo-session-8.png",
+    },
+    {
+        title: "Spring Integration",
+        description: "Supports the well-known Enterprise Integration Patterns through lightweight messaging and declarative adapters.",
+        imageSrc: "assets/images/card-images/spring-integration-9.svg",
+    },
+    {
+        title: "Spring HATEOAS",
+        description: "Simplifies creating REST representations that follow the HATEOAS principle.",
+        imageSrc: "assets/images/card-images/spring-hateoas-10.svg",
+    },
+    {
+        title: "Spring REST Docs",
+        description: "Lets you document RESTful services by combining hand-written documentation with auto-generated snippets produced with Spring MVC Test or REST Assured.",
+        imageSrc: "assets/images/card-images/spring-restdocs-11.png",
+    },
+    {
+        title: "Spring Batch",
+        description: "Simplifies and optimizes the work of processing high-volume batch operations.",
+        imageSrc: "assets/images/card-images/spring-batch-12.svg",
+    },
+    {
+        title: "Spring AMQP",
+        description: "Applies core Spring concepts to the development of AMQP-based messaging solutions.",
+        imageSrc: "assets/images/card-images/spring-amqp-13.svg",
+    },
+    {
+        title: "Spring CredHub",
+        description: "Provides client-side support for storing, retrieving, and deleting credentials from a CredHub server running in a Cloud Foundry platform.",
+        imageSrc: "assets/images/card-images/spring-14.svg",
+    },
+    {
+        title: "Spring Flo",
+        description: "Provides a JavaScript library that offers a basic embeddable HTML5 visual builder for pipelines and simple graphs.",
+        imageSrc: "assets/images/card-images/spring-flo-15.svg",
+    },
+    {
+        title: "Spring for Apache Kafka",
+        description: "Provides Familiar Spring Abstractions for Apache Kafka.",
+        imageSrc: "assets/images/card-images/spring-kafka-16.svg",
+    },
+    {
+        title: "Spring LDAP",
+        description: "Simplifies the development of applications that use LDAP by using Spring's familiar template-based approach.",
+        imageSrc: "assets/images/card-images/spring-ldap-17.svg",
+    },
+    {
+        title: "Spring Shell",
+        description: "Makes writing and testing RESTful applications easier with CLI-based resource discovery and interaction.",
+        imageSrc: "assets/images/card-images/spring-shell-18.svg",
+    },
+    {
+        title: "Spring Statemachine",
+        description: "Provides a framework for application developers to use state machine concepts with Spring applications.",
+        imageSrc: "assets/images/card-images/spring-statemachine-19.svg",
+    },
+    {
+        title: "Spring Vault",
+        description: "Provides familiar Spring abstractions for HashiCorp Vault",
+        imageSrc: "assets/images/card-images/spring-20.svg",
+    },
+    {
+        title: "Spring Web Flow",
+        description: "Supports building web applications that feature controlled navigation, such as checking in for a flight or applying for a loan.",
+        imageSrc: "assets/images/card-images/spring-web-flo-21.svg",
+    },
+    {
+        title: "Spring Web Services",
+        description: "Facilitates the development of contract-first SOAP web services.",
+        imageSrc: "assets/images/card-images/logo-web-services-22.svg",
+    },
+];
+
+let header = document.querySelector('.menu__list');
+
+for(let item of headerMenu){
+    let menuItem = document.createElement('li');
+    menuItem.className = "menu__items";
+    if(item.dropdown){
+        let dropdownContent = document.createElement('div');
+        dropdownContent.className = "dropdown-content";
+        for(let links of item.dropdown){
+            let link = document.createElement('a');
+            link.href = links.href;
+            link.innerHTML = links.text;
+            dropdownContent.append(link);
+        }
+
+        let dropdownButton = document.createElement('div');
+        dropdownButton.className = 'dropdown';
+        dropdownButton.innerHTML = `
+            <span class="dropbtn">${item.title}</span>
+            <div class="nav-arrow"></div>
+        `;
+
+        menuItem.append(dropdownContent);
+        menuItem.append(dropdownButton);
+    } else{
+        menuItem.innerHTML = `<a href="${item.href}">${item.title}</a>`
+    }
+    header.append(menuItem);
+}
